0caef52ea259a8e9a506fd1d1709ea07b80ed08d
mikduk <43962633+mikduk@users.noreply.github.com>
2019-04-07 23:58:48 +0200

    Wersja Mac

--- ASD/Lista2/funkcjePomocnicze.h
+++ ASD/Lista2/funkcjePomocnicze.h
@@ -19,12 +19,13 @@ typedef struct statystyki{
 }Statystyki;
 
 void pokazTablice(int n, int tablica[]);
+bool dobryPorzadek(int n, int * tablica, bool asc);
 void zamien (int a, int b, int * tablica);
 void wstawIPrzesun(int wart, int pozycja, int n, int * tablica);
-int wstawIPrzestaw(int wart, int pozycja, int n, int * tablica, int przestawienia);
-int porownanie(int porownania, int a, int b, char znak);
-int porownanie(int porownania, int a, int b, char znak, std::string reszta);
-int przestawienie(int przestawienia, int a, int b);
-int przestawienie(int przestawienia, int a, std::string b);
+int wstawIPrzestaw(bool pokaz, int wart, int pozycja, int n, int * tablica, int przestawienia);
+int porownanie(bool pokaz, int porownania, int a, int b, char znak);
+int porownanie(bool pokaz, int porownania, int a, int b, char znak, std::string reszta);
+int przestawienie(bool pokaz, int przestawienia, int a, int b);
+int przestawienie(bool pokaz, int przestawienia, int a, std::string b);
 
 #endif /* funkcjePomocnicze_h */
